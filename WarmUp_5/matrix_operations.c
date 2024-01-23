c581a0eeea6063de64829c1f034843a6481fead6
Luke Niemeyer <ltniemo@gmail.com>
2024-01-23 05:31:36 +0000

    Added WarmUp_5 to repository

---
+++ WarmUp_5/matrix_operations.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void addMatrices(int rows, int cols, int a[rows][cols], int b[rows][cols], int sum[rows][cols]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+void multiplyMatrices(int rowsA, int colsA, int colsB, int a[rowsA][colsA], int b[colsA][colsB], int product[rowsA][colsB]) {
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsB; j++) {
+            product[i][j] = 0;
+            for (int k = 0; k < colsA; k++) {
+                product[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(int rows, int cols, int matrix[rows][cols]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int firstMatrix[2][2] = {{1,2}, {3,4}};
+    int secondMatrix[2][2] = {{5,6}, {7,8}};
+    int sum[2][2]; 
+    int product[2][2];
+
+    addMatrices(2, 2, firstMatrix, secondMatrix, sum);
+
+    multiplyMatrices(2, 2, 2, firstMatrix, secondMatrix, product);
+
+    printf("Matrix Addition:\n");
+    printMatrix(2, 2, sum);
+
+    printf("Matrix Multiplication:\n");
+    printMatrix(2, 2, product);
+
+    return 0;
+}
